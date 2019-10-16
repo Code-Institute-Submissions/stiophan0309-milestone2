bc8309c94f684fe18b14e1863346f122e44522f6
stiophan0309 <steven.a.horne@btinternet.com>
2019-10-16 13:27:56 +0000

    Added locations to JS and map for Accomodation section

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -10,10 +10,16 @@ function initMap() {
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 57.038273, lng: -5.685858 }, /* Inverie Pier */
-        { lat: 57.040790, lng: -5.697839 }, /* The Old Forge */
-        { lat: 57.103380, lng: -5.380410 }, /* Kinlochhourn  */
-        { lat: 56.970180, lng: -5.321517 }, /* Strathan */
+        { lat: 57.038273, lng: -5.685858 }, /* A Inverie Pier */
+        { lat: 57.037474, lng: -5.682667 }, /* B The Old Forge */
+        { lat: 57.103380, lng: -5.380410 }, /* C Kinlochhourn  */
+        { lat: 56.970180, lng: -5.321517 }, /* D Strathan */
+        { lat: 57.028766, lng: -5.661768 }, /* E Kilchoan Estate */
+        { lat: 57.082106, lng: -5.767767 }, /* F Airor */
+        { lat: 57.040644, lng: -5.697784 }, /* G Creag Eiridh */
+        { lat: 57.037327, lng: -5.730222 }, /* H Glaschoille House */
+        { lat: 57.031709, lng: -5.666478 }, /* I The Old Byre, Knoydart House */
+        { lat: 57.032765, lng: -5.670364 }, /* J Knoydart Foundation Bunkhouse & Campsite */
         { lat: 57.074732, lng: -5.592170 }, /* Ladhar Bheinn */
         { lat: 57.049542, lng: -5.516111 }, /* Luinne Bheinn */
         { lat: 57.032051, lng: -5.545687 }, /* Meall Buidhe */
@@ -25,7 +31,7 @@ function initMap() {
             position: location,
             label: labels[i % labels.length]
         });
-    }); 
+    });
 
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
