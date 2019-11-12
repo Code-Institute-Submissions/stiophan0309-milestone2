19c29772df8179e8f65a02fa593e3177d5299ea5
stiophan0309 <steven.a.horne@btinternet.com>
2019-11-12 21:04:52 +0000

    Created Accommodation.js and moved code from home page, renamed and moved JSON file to assets/js/

---
+++ assets/js/accommodation.js
@@ -0,0 +1,27 @@
+        fetch('assets/js/accommodation.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+        function appendData(data) {
+            var mainContainer = document.getElementsByClassName('card-deck')[0];
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("div");
+                div.innerHTML =  '<div class="card h-100 card mb-3">' +
+                                 '<img class="card-img-top img-fluid" src="' + data[i].image + '" class="card-img-top" alt="' + data[i].name + '">' + 
+                                 '<div class="card-body">' + 
+                                    '<h5 class="card-title">' + data[i].name + '</h5>' +
+                                    '<p class="card-text">' + data[i].type + '</p>' +
+                                    '<p class="card-text">' + data[i].description + '</p>' + 
+                                    '<a href="' + data[i].url + '" class="btn btn-info" target="_blank">' + 'Home Page' + '</a>' + 
+                                 '</div>' + 
+                                 '</div>';
+
+                mainContainer.appendChild(div);
+            }
+        }
