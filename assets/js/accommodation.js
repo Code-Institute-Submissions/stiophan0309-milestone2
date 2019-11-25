6b8bfa645560bb48c053079b30735665b8b4bac8
stiophan0309 <steven.a.horne@btinternet.com>
2019-11-25 16:40:43 +0000

    Sorted layout and alignmemnt issues with card deck

--- assets/js/accommodation.js
+++ assets/js/accommodation.js
@@ -9,18 +9,21 @@
                 console.log('error: ' + err);
             });
         function appendData(data) {
-            var mainContainer = document.getElementsByClassName('card-deck')[0];
+            var mainContainer = document.getElementById('cards');
             for (var i = 0; i < data.length; i++) {
                 var div = document.createElement("div");
-                div.innerHTML =  '<div class="card h-100 card mb-3">' +
-                                 '<img class="card-img-top img-fluid" src="' + data[i].image + '" class="card-img-top" alt="' + data[i].name + '">' + 
-                                 '<div class="card-body">' + 
-                                    '<h5 class="card-title">' + data[i].name + '<span class="badge badge-danger letter">' + data[i].letter + '</span></h5>' +
-                                    '<p class="card-text">' + data[i].type + '</p>' +
-                                    '<p class="card-text">' + data[i].description + '</p>' + 
-                                    '<a href="' + data[i].url + '" class="btn btn-info" target="_blank">' + 'Home Page' + '</a>' + 
-                                 '</div>' + 
-                                 '</div>';
+                div.innerHTML =  div.innerHTML = `
+                                                <div class="card">
+                                                    <img class="card-img-top img-fluid" src="${data[i].image}"alt="${data[i].name}">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">${data[i].name}<span class="badge badge-danger letter">${data[i].letter}</span></h5>
+                                                        <p class="card-text">${data[i].type}</p><p class="card-text">${data[i].description}</p>
+                                                   </div>
+                                                    <div class="card-footer">
+                                                        <a href="${data[i].url}" class="btn btn-info" target="_blank">Home Page</a>
+                                                    </div>
+  
+                                                </div>`
 
                 mainContainer.appendChild(div);
             }
