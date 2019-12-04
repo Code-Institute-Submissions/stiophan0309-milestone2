d9c97c5d4c8cf886022f4bc37b077ba5b9db0eca
stiophan0309 <steven.a.horne@btinternet.com>
2019-12-04 16:18:53 +0000

    renamed navbar.js to header.js and removed navbar offset code due to malfunction

--- assets/js/header.js
+++ assets/js/header.js
@@ -10,14 +10,3 @@
                 }
             });
         });
-
-$(".nav-item a[href^='#']").on('click', function(e) {
-   // prevent default anchor click behavior
-   e.preventDefault();
-
-   // animate
-   $('html, body').animate({
-       scrollTop: $(this.hash).offset().top - 150
-     }, 300, function(){
-     });
-});
